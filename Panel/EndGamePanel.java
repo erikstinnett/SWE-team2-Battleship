85dc4c64d500491e1394b01a295ffbe0349fa73c
Vincent Truong <112707277+Shenshii@users.noreply.github.com>
2023-11-17 16:06:32 -0600

    Update EndGamePanel.java

--- Panel/EndGamePanel.java
+++ Panel/EndGamePanel.java
@@ -1,13 +1,73 @@
 package Panel;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class EndGamePanel extends JPanel {
-    // Assuming EndGamePanel has components and behavior that need to be implemented
+class EndGamePanel extends JPanel {
+    private EndGameControl control;
 
-    public EndGamePanel() {
-        // Initialize end game information
+    public EndGamePanel(EndGameControl ec) {
+        this.control = ec; // Store the reference to the EndGameControl object
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        // Determine the winner and create a message
+        String resultText = control.didPlayerWin() ? "Congratulations, You Won!" : "You Lost, Opponent Won!";
+        JLabel resultLabel = new JLabel(resultText, JLabel.CENTER);
+        resultLabel.setFont(new Font("Serif", Font.BOLD, 24));
+        resultLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Replay button
+        JButton replayButton = new JButton("Play Again");
+        replayButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        replayButton.addActionListener(e -> control.restartGame());
+
+        // Exit button
+        JButton exitButton = new JButton("Exit to Main Menu");
+        exitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        exitButton.addActionListener(e -> control.exitToMainMenu());
+
+        // Add components to the panel
+        add(Box.createVerticalGlue());
+        add(resultLabel);
+        add(Box.createRigidArea(new Dimension(0, 20))); // Spacer
+        add(replayButton);
+        add(Box.createRigidArea(new Dimension(0, 10))); // Spacer
+        add(exitButton);
+        add(Box.createVerticalGlue());
     }
 
-    // Methods related to end of the game
+    // Main method for testing the panel
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("End Game Panel Test");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.add(new EndGamePanel(new EndGameControl()));
+            frame.pack();
+            frame.setLocationRelativeTo(null); // Center the window
+            frame.setVisible(true);
+        });
+    }
+
+    // Additional methods and inner classes (if any)...
 }
+
+class EndGameControl {
+    public boolean didPlayerWin() {
+        // Logic to determine if the player won
+        return true; // Placeholder for actual win determination logic
+    }
+
+    public void restartGame() {
+        // Logic to restart the game
+        System.out.println("Restarting game...");
+    }
+
+    public void exitToMainMenu() {
+        // Logic to exit to the main menu
+        System.out.println("Exiting to the main menu...");
+    }
+
+    // Additional methods and inner classes (if any)...
+}
+
+// No separate test class is needed as the main method is included in the EndGamePanel class

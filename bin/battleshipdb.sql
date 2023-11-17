41a02fddc9c15701de1f179c0ba0accd805c54a1
Dakota Leslie <dleslie@cub.uca.edu>
2023-11-17 11:55:40 -0600

    trying to remove binaries, and keep them from making it into repo

--- bin/battleshipdb.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE gameData
-(username VARCHAR (20),
-password VARBINARY (16) NOT NULL,
-wins INTEGER (100),
-losses INTEGER (100),
-CONSTRAINT gameData_username_pk PRIMARY KEY (username));

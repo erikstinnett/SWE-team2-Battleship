4fa1e93250056a93eb7f69b345930714fc699568
AshtonJohns <91106427+AshtonJohns@users.noreply.github.com>
2023-11-19 21:50:21 -0600

    Update MenuPanel.java - Vincent

--- Panel/MenuPanel.java
+++ Panel/MenuPanel.java
@@ -1,38 +1,47 @@
 package Panel;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.;
+import java.awt.;
 
 public class MenuPanel extends JPanel {
-	
+
     public MenuPanel(MenuControl mc) {
-    	//Create Main Menu Battleship TitleLabel
-    	//Can be adjusted accordingly later
-    	JLabel titleLabel = new JLabel("BATTLESHIP", SwingConstants.CENTER);
-    	titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
-    	titleLabel.setForeground(Color.BLUE);
-    	
-    	//Create Buttons
-    	JButton loginButton = new JButton("Login!");
-    	//loginButton.addActionListener(mc);
-    	JPanel loginButtonBuffer = new JPanel();
-    	loginButtonBuffer.add(loginButton);
-    	
-    	// Create the exit button.
+        //Create Main Menu Battleship TitleLabel
+        //Can be adjusted accordingly later
+        JLabel titleLabel = new JLabel("BATTLESHIP", SwingConstants.CENTER);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        titleLabel.setForeground(Color.BLUE);
+
+        //Create Buttons
+        JButton playButton = new JButton("Play!");
+        //loginButton.addActionListener(mc);
+        JPanel loginButtonBuffer = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        loginButtonBuffer.add(playButton);
+
+        // Create the exit button.
         JButton exitButton = new JButton("Exit");
         //exitButton.addActionListener(mc);
-        JPanel exitButtonBuffer = new JPanel();
+        JPanel exitButtonBuffer = new JPanel(new FlowLayout(FlowLayout.CENTER));
         exitButtonBuffer.add(exitButton);
-        
+
+        //Create the view scoreboard button.
+
+        JButton viewScoreboard = new JButton("View Scoreboard");
+        JPanel viewScoreboardBuffer = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        viewScoreboardBuffer.add(viewScoreboard);
+
         // Arrange the components in a grid.
-        JPanel grid = new JPanel(new GridLayout(3, 1, 5, 5));
+        JPanel grid = new JPanel(new GridLayout(4, 1, 5, 5));
         grid.add(titleLabel);
         grid.add(loginButtonBuffer);
         grid.add(exitButtonBuffer);
-        
-        this.add(grid);	
+        grid.add(viewScoreboardBuffer);
+
+        setPreferredSize(new Dimension(400,200));
+
+        this.add(grid);
     }
-    
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
@@ -57,5 +66,7 @@ public class MenuPanel extends JPanel {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
-    
+
+
+
 }

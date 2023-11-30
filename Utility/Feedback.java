1a93f5c361513e2b02567ab9c463d64c7a7eb86f
AshtonJohns <ashtonmjohns@gmail.com>
2023-11-30 03:28:01 -0600

    has detailed message field, e.g., ship being sunk feedback

--- Utility/Feedback.java
+++ Utility/Feedback.java
@@ -7,6 +7,8 @@ public class Feedback implements Serializable
   // Data field for storing the feedback message and type.
   private String message;
   private String type;
+  // this field is for detailed messages, e.g., which ship was sunk
+  private String detailed_msg;
   
   // Getters for the feedback message and type.
   public String getMessage()
@@ -26,6 +28,15 @@ public class Feedback implements Serializable
   public void setType(String type)
   {
     this.type = type;
+  }
+
+  // Setter and Getter for extra detailed message
+  public void setDetailedMessage(String detailed_msg){
+    this.detailed_msg = detailed_msg;
+  }
+
+  public String getDetailedMessage(){
+    return this.detailed_msg;
   }
   
   // Constructor for creating a new feedback object with a message and type.

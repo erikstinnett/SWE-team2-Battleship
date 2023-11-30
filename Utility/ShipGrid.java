6c0a0c8d017719a5186e77ad16bcb4c1f36ce379
Dakota Leslie <dleslie@cub.uca.edu>
2023-11-29 19:28:04 -0600

    grids should be functional (praying)

--- Utility/ShipGrid.java
+++ Utility/ShipGrid.java
@@ -18,8 +18,78 @@ public class ShipGrid extends Grid{
 		return super.getGrid();
 	}
 
-	public void update(int[] lastShot, String type){
+	public Feedback update(int[] lastShot, String type){
 		//type = hit or miss
-		super.updateGrid(lastShot, type);
+		//super.updateGrid(lastShot, type);
+		grid = super.getGrid();
+		int whatThere = grid[lastShot[0]][lastShot[1]];
+		Feedback feedback = new Feedback("","");
+		
+		switch (whatThere) {
+				
+			case 20:
+				feedback.setMessage("You hit a ship!");
+				feedback.setType("hit");
+				super.hit(lastShot);
+				for (Ship i : ships) {
+					if (i.getID() == 20) {
+						i.removeLife();
+					}
+				}
+					
+				break;
+				
+			case 21:
+				feedback.setMessage("You hit a ship!");
+				feedback.setType("hit");
+				super.hit(lastShot);
+				for (Ship i : ships) {
+					if (i.getID() == 20) {
+						i.removeLife();
+					}
+				}
+				break;
+				
+			case 22:
+				feedback.setMessage("You hit a ship!");
+				feedback.setType("hit");
+				super.hit(lastShot);
+				for (Ship i : ships) {
+					if (i.getID() == 20) {
+						i.removeLife();
+					}
+				}
+				break;
+			
+			case 23:
+				feedback.setMessage("You hit a ship!");
+				feedback.setType("hit");
+				super.hit(lastShot);
+				for (Ship i : ships) {
+					if (i.getID() == 20) {
+						i.removeLife();
+					}
+				}
+				break;
+				
+			case 24:
+				feedback.setMessage("You hit a ship!");
+				feedback.setType("hit");
+				super.hit(lastShot);
+				for (Ship i : ships) {
+					if (i.getID() == 20) {
+						i.removeLife();
+					}
+				}
+				break;
+				
+			default:
+				feedback.setMessage("You missed!");
+				feedback.setType("miss");
+				super.miss(lastShot);
+				break;
+		}
+		
+		return feedback;
 	}
 }
--- Utility/ShootGrid.java
+++ Utility/ShootGrid.java
@@ -8,14 +8,39 @@ public class ShootGrid extends Grid{
 		super();
 		
 	}
-
-	public void update(int[] lastShot){
-
-		super.updateGrid(lastShot, "ShootGrid");
-		//return parent grid
-		// shootGrid = super.getGrid();
-	}
 	
+	public Feedback validate(int[] shot) {
+		Feedback feedback = new Feedback("","");
+		
+		grid = super.getGrid();
+		int WhatThere = grid[shot[0]][shot[1]];
+		
+		switch (WhatThere) {
+			case 1:
+				feedback.setMessage("This was already hit");
+				feedback.setType("invalidShot");
+				break;
+				
+			case 2:
+				feedback.setMessage("This was already missed");
+				feedback.setType("invalidShot");
+				break;
+								
+			default:
+				feedback.setMessage("This is a valid shot");
+				feedback.setType("validShot");
+				break;
+		}
+		return feedback;
+	}
+
+//	public void update(int[] lastShot){
+//
+//		super.updateGrid(lastShot, "ShootGrid");
+//		//return parent grid
+//		// shootGrid = super.getGrid();
+//	}
+//	
 	public int[][] getGrid(){
 		return super.getGrid();
 	}
